ffcc988630d44d12d7d047e2906dac47e584104c
Choo King <choocodes@gmail.com>
2024-08-26 16:41:04 -0400

    Split code into separate Program and Window files, and replicate original comments.

--- CreatingWindow/Program.fs
+++ CreatingWindow/Program.fs
@@ -1,28 +1,22 @@
-﻿module OpenTKTest
+﻿module LearnOpenTK
 
-open OpenTK.Graphics.OpenGL4
 open OpenTK.Mathematics
 open OpenTK.Windowing.Common
 open OpenTK.Windowing.Desktop
+open Window
 
-type Window(gameWindowSettings, nativeWindowSettings) =
-    inherit GameWindow(gameWindowSettings, nativeWindowSettings)
-    override this.OnLoad () =
-        GL.ClearColor(0.0f, 0.0f, 1.0f, 0.0f)
-        base.OnLoad()
-        
-    override this.OnResize e =
-        GL.Viewport(0, 0, e.Width, e.Height)
-        base.OnResize e
 
-    override this.OnRenderFrame(e) =
-        GL.Clear(ClearBufferMask.ColorBufferBit)
-        this.Context.SwapBuffers()
-        base.OnRenderFrame(e)
 
 [<EntryPoint>]
 let main argv =
-    let nativeSettings = new NativeWindowSettings(ClientSize = new Vector2i(1600, 1200), Title = "Testing", Flags = ContextFlags.ForwardCompatible)    
+    let nativeSettings =
+        new NativeWindowSettings(
+            ClientSize = new Vector2i(800, 600),
+            Title = "LearnOpenTK - Creating a Window",
+            // This is needed to run on macos
+            Flags = ContextFlags.ForwardCompatible
+        )
+    // To create a new window, create a class that extends GameWindow, then call Run() on it.
     use window = new Window(GameWindowSettings.Default, nativeSettings)
     window.Run()
     0
---
+++ CreatingWindow/Window.fs
@@ -0,0 +1,15 @@
+﻿module Window
+
+open OpenTK.Windowing.Desktop
+open OpenTK.Windowing.GraphicsLibraryFramework
+
+type Window(gameWindowSettings, nativeWindowSettings) =
+    inherit GameWindow(gameWindowSettings, nativeWindowSettings)
+    
+    // This function runs on every update frame.
+    override this.OnUpdateFrame(e) =
+        // Check if the Escape button is currently being pressed.
+        if (this.KeyboardState.IsKeyDown Keys.Escape) then
+            // If it is, close the window.
+            this.Close()
+        base.OnUpdateFrame(e)            
