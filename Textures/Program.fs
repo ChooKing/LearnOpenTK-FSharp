c2dfe9465f084682b822f4814486e01254c96844
Choo King <choocodes@gmail.com>
2024-09-13 17:01:29 -0400

    Texture example fully translated to F#.

---
+++ Textures/Program.fs
@@ -0,0 +1,19 @@
+﻿module Textures
+open OpenTK.Mathematics
+open OpenTK.Windowing.Common
+open OpenTK.Windowing.Desktop
+open Window
+
+[<EntryPoint>]
+let main argv =
+    let nativeSettings =
+        new NativeWindowSettings(
+            ClientSize = new Vector2i(800, 600),
+            Title = "LearnOpenTK - Textures",
+            // This is needed to run on macos
+            Flags = ContextFlags.ForwardCompatible
+        )
+    // To create a new window, create a class that extends GameWindow, then call Run() on it.
+    use window = new Window(GameWindowSettings.Default, nativeSettings)
+    window.Run()
+    0
